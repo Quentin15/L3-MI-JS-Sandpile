3679dda1994c814f37f5785e98d955bff7ebbf92
Admin <Admin@LAPTOP-TPQUP5TP>
2024-04-22 00:46:12 +0200

    Next Voronoï Diagram

--- js/TilingPresets/VoronoiDiagram.js
+++ js/TilingPresets/VoronoiDiagram.js
@@ -20,10 +20,10 @@ function sqTile(x, y){
 	var id = [x, y];
 	
 	var bounds = [];
-	bounds.push(x, y); // Gauche bas
-	bounds.push(x+0.2, y); // Droite bas
-	bounds.push(x+0.2, y+0.2); // Droit haut
-	bounds.push(x, y+0.2); // Gauche haut
+	bounds.push(x-0.1, y-0.1); // Gauche bas
+	bounds.push(x+0.1, y-0.1); // Droite bas
+	bounds.push(x+0.1, y+0.1); // Droit haut
+	bounds.push(x-0.1, y+0.1); // Gauche haut
 	
 	return new Tile(id, [], bounds, 4);
 }
